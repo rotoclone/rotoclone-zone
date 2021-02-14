53e6c01459d9d33cce98309dcc1686a382b26f8e
rotoclone <rotoclone@gmail.com>
2021-02-14 16:56:08 -0600

    Add template stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
+use rocket_contrib::templates::Template;
+use serde::Serialize;
+
 #[macro_use]
 extern crate rocket;
 
+#[derive(Serialize)]
+struct IndexContext {
+    title: String,
+    header: String,
+    items: Vec<String>,
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, World!"
+fn index() -> Template {
+    greeting("there".to_string())
+}
+
+#[get("/<name>")]
+fn greeting(name: String) -> Template {
+    let context = IndexContext {
+        title: "Sup".to_string(),
+        header: format!("Hello {}", name),
+        items: vec!["boop".to_string(), "doop".to_string(), "floop".to_string()],
+    };
+    Template::render("index", &context)
 }
 
 #[launch]
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index])
+    rocket::ignite()
+        .mount("/", routes![index, greeting])
+        .attach(Template::fairing())
 }
