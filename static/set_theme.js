942071deeb514a013a1d2485fb3537e06ad4ebfd
rotoclone <rotoclone@gmail.com>
2021-04-11 03:19:58 -0500

    Move font files into their own directory

--- static/set_theme.js
+++
@@ -1,6 +0,0 @@
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") {
-    document.body.classList.toggle("dark-theme");
-} else if (currentTheme == "light") {
-    document.body.classList.toggle("light-theme");
-}
--- static/theme_toggle.js
+++ static/theme_toggle.js
@@ -1,12 +1,9 @@
 // from https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
 
-// Select the button
 const btn = document.querySelector(".theme-toggle");
 
-// Listen for a click on the button
 btn.addEventListener("click", function () {
     document.body.classList.toggle("light-theme");
     var theme = document.body.classList.contains("light-theme") ? "light" : "dark";
-    // Finally, let's save the current preference to localStorage to keep using it
     localStorage.setItem("theme", theme);
 });
