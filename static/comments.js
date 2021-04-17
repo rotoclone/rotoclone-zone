173352b64a49b103463b771a20597f556445ebd4
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-04-17 13:59:35 -0500

    Add custom style for Commento (#7)
    * Add commento and do some stylin
    
    * Commento stylin
    
    * Use commento++ and add a bunch of dumb scripts everywhere
    
    * Clean up some script tags
    
    * Hide anonymous checkbox
    
    * More stylin
    
    * Fix comment permalinks and add icons
    
    * Use icons
    
    * Use arrows in blog index too

---
+++ static/comments.js
@@ -0,0 +1,27 @@
+const showCommentsButton = document.querySelector(".show-comments-button");
+
+window.showComments = function () {
+    window.commento.main();
+    showCommentsButton.style.display = "none";
+}
+
+window.loadedCommento = function () {
+    if (window.location.href.includes("#commento")) {
+        window.showComments();
+        document.querySelector(window.location.href.split("#")[1]).scrollIntoView();
+    } else {
+        showCommentsButton.addEventListener("click", function () {
+            window.showComments();
+        });
+    }
+}
+
+window.showCommentsButtonText = function (count) {
+    if (count === 0) {
+        return "Make a comment";
+    } else if (count === 1) {
+        return "Show 1 comment";
+    } else {
+        return "Show " + count + " comments";
+    }
+}
